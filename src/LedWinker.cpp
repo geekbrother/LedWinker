84a1ab52e327b781de3b03f136b0f01b16beae78
Max Kalashnikov <geekbrother@users.noreply.github.com>
2020-07-10 16:32:25 +0300

    Create LedWinker.cpp

---
+++ src/LedWinker.cpp
@@ -0,0 +1,58 @@
+#include "Arduino.h"
+#include "LedWinker.hpp"
+
+#define SLOW_BLINK_DELAY 900
+#define FAST_BLINK_DELAY 180
+
+LedWinker::LedWinker(int GPIO){
+    _GPIO = GPIO;
+    // Set to default LOW (off) the LED
+    pinMode(GPIO, OUTPUT);
+    digitalWrite(_GPIO, LOW);
+}
+
+void LedWinker::Wink(winkType TYPE){
+    // Just a setter
+    _winkType = TYPE;
+}
+
+void LedWinker::Loop(){
+    // Check for type
+    switch (_winkType) {
+        case SLOW:
+            checkBlinkTime(SLOW_BLINK_DELAY);
+            break;
+        case FAST:
+            checkBlinkTime(FAST_BLINK_DELAY);
+            break;
+        case ON:
+            if(_lastState == LOW){
+                _lastState = HIGH;
+                digitalWrite(_GPIO, _lastState);
+            };
+            break;
+        case OFF:
+            if(_lastState == HIGH){
+                _lastState = LOW;
+                digitalWrite(_GPIO, _lastState);
+            };
+            break;
+    };
+}
+
+// Check is time to ON or OFF led based on delay
+void LedWinker::checkBlinkTime(int DELAY){
+    const unsigned long currentMillis = millis();
+    if (currentMillis - _lastBlinkedTime >= DELAY) {
+        // save the last time you blinked the LED
+        _lastBlinkedTime = currentMillis;
+        // if the LED is off turn it on and vice-versa:
+        if (_lastState == LOW) {
+            _lastState = HIGH;
+        } else {
+            _lastState = LOW;
+        }
+        // set the LED with the ledState of the variable:
+        digitalWrite(_GPIO, _lastState);
+    }
+}
